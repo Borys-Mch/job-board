2eb55d235ba07c454c536db5bfe82e077230e2e1
Borys Mch <borys.mch@gmail.com>
2024-09-11 11:58:40 +0300

    Added JobsRow component

--- src/app/components/Jobs.tsx
+++ src/app/components/Jobs.tsx
@@ -1,13 +1,17 @@
+import JobsRow from "./JobRow";
+
 export default function Jobs() {
     return (
         <div className="bg-slate-200 py-4">
             <div className="container">
                 <h2 className="font-bold mb-4">Recent jobs</h2>
                 <div className="flex flex-col gap-4">
-                    <div className="bg-white p-6 rounded-md shadow-md"></div>
-                    <div className="bg-white p-6 rounded-md shadow-md"></div>
-                    <div className="bg-white p-6 rounded-md shadow-md"></div>
-                    <div className="bg-white p-6 rounded-md shadow-md"></div>
+                    <JobsRow />
+                    <JobsRow />
+                    <JobsRow />
+                    <JobsRow />
+                    <JobsRow />
+                    <JobsRow />
                 </div>
             </div>
         </div>
