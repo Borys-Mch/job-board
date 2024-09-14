3c22de8109cf51833db868657c6f22be1d2482cd
Borys Mch <borys.mch@gmail.com>
2024-09-14 10:49:18 +0300

    Added like icon in JobRow

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,7 +1,13 @@
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function JobsRow() {
     return (
         <>
-            <div className="bg-white p-4 rounded-md shadow-md md:flex">
+            <div className="bg-white p-4 rounded-md shadow-md md:flex relative">
+                <div className=" absolute cursor-pointer top-4 right-4">
+                    <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} />
+                </div>
                 <div className="flex grow gap-4">
                 <div className="content-center">
                     <img className="size-12" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="" />
