7e8a54c112536dd8ca9066da84d4bfa98dbcf59f
Borys Mch <borys.mch@gmail.com>
2024-09-14 10:30:54 +0300

    Added mobile responsive JobsRow component

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,19 +1,23 @@
 export default function JobsRow() {
     return (
         <>
-            <div className="bg-white p-4 rounded-md shadow-md flex gap-4">
+            <div className="bg-white p-4 rounded-md shadow-md md:flex">
+                <div className="flex grow gap-4">
                 <div className="content-center">
                     <img className="size-12" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="" />
                 </div>
-                <div className="grow">
-                    <div className="text-gray-500 text-sm">Pinterest</div>
-                    <div className="font-bold text-lg mb-1">FrontEnd</div>
-                    <div className="text-gray-400 text-sm">
-                        Remove &middot; New York, US &middot; Full-time
+                <div className="grow sm:flex">
+                    <div className="grow">
+                        <div className="text-gray-500 text-sm">Pinterest</div>
+                        <div className="font-bold text-lg mb-1">FrontEnd</div>
+                        <div className="text-gray-400 text-sm">
+                            Remove &middot; New York, US &middot; Full-time
+                        </div>
+                    </div>
+                    <div className="content-end text-gray-500 text-sm">
+                        2 weeks
                     </div>
                 </div>
-                <div className="content-end text-gray-500 text-sm">
-                    2 weeks
                 </div>
             </div>
         </>
