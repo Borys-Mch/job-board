bcbbd59b6a2f18a1237c434117cc6f7f4ff2665f
Borys Mch <borys.mch@gmail.com>
2024-09-15 20:19:02 +0300

    Added Authentication

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,30 @@
+import { getSignInUrl, getUser, signOut, } from '@workos-inc/authkit-nextjs';
 import Link from "next/link";
 
-export default function Header() {
+export default async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
     return (
         <header>
         <div className="container flex items-center justify-between my-4">
         <Link href={'/'} className="font-bold text-xl">Job Board</Link>
-        <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
-          <Link href={'/login'} className="bg-gray-200">Login</Link>
-          <Link href={'/new-listing'} className="bg-blue-600 text-white">Post a job</Link>
+        <nav className="flex gap-2">
+          {!user && (
+          <Link href={signInUrl} className="bg-gray-200 rounded-md py-2 px-4">Login</Link>
+          )}
+          {user && (
+            <form action={async () => {
+              'use server';
+              await signOut();
+            }}>
+              <button
+              type='submit'
+              className="bg-gray-200 rounded-md py-2 px-4">
+                Logout
+              </button>
+            </form>
+          )}
+          <Link href={'/new-listing'} className="bg-blue-600 text-white rounded-md py-2 px-4">Post a job</Link>
         </nav>
         </div>
       </header>
