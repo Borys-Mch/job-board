f346c1802b9f981f3217e52bdf8d8e7da1dcd25a
Borys Mch <borys.mch@gmail.com>
2024-09-19 12:09:55 +0300

    Added actiom for Create company button

---
+++ src/app/actions/workosActions.ts
@@ -0,0 +1,14 @@
+'use server'
+import { WorkOS } from "@workos-inc/node"
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY)
+
+export async function createCompany(companyName: string, userId:string) {
+    'use server';
+    const org = await workos.organizations.createOrganization({name: companyName})
+    return await workos.userManagement.createOrganizationMembership({
+        userId,
+        organizationId: org.id,
+        roleSlug: 'admin',
+    })
+}
--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,9 +1,19 @@
-
 import { getUser } from "@workos-inc/authkit-nextjs"
 import { WorkOS } from "@workos-inc/node"
+import { createCompany } from "../actions/workosActions"
 
 export default async function NewListingPage() {
+
+    const workos = new WorkOS(process.env.WORKOS_API_KEY)
+
     const {user} = await getUser()
+
+    async function handleNewCompanyFormSubmit(data:FormData) {
+        'use server'
+        if (user) {
+            await createCompany(data.get('newCompanyName') as string, user.id)
+        }
+    }
 
     if (!user) {
         return (
@@ -12,8 +22,6 @@ export default async function NewListingPage() {
             </div>
         )
     }
-
-    const workos = new WorkOS(process.env.WORKOS_API_KEY)
 
     const organizationMemberships = await workos.userManagement.listOrganizationMemberships({
         userId: user?.id
@@ -34,9 +42,15 @@ export default async function NewListingPage() {
 
                 <h2 className="text-lg mt-6">Create a new company</h2>
                 <p className="text-gray-500 text-sm mb-2">To create a job listing your first need to register a company</p>
-                <form action='' className="flex gap-2">
-                    <input className="p-2 border border-gray-400 rounded-md" type="text" placeholder="company name" />
-                    <button className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md">
+                <form 
+                    action={handleNewCompanyFormSubmit} 
+                    className="flex gap-2">
+                    <input 
+                        name="newCompanyName"
+                        className="p-2 border border-gray-400 rounded-md" 
+                        type="text" 
+                        placeholder="company name" />
+                    <button type="submit" className="flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md">
                         Create company
                     </button>
                 </form>
